bf65dbdca8a9a99db373631ddb8efde5396ae1bc
SnowSurfer <drinkaziu@gmail.com>
2019-04-28 17:46:25 -0400

    display colors in SingleColorPalette

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -14,7 +14,7 @@ class ColorBox extends Component {
     });
   }
   render() {
-    const { name, background, paletteId, colorId } = this.props;
+    const { name, background, paletteId, colorId, showLink } = this.props;
     const { copied } = this.state;
     return (
       <CopyToClipboard  text={background} onCopy={this.changeCopyState}>
@@ -30,9 +30,10 @@ class ColorBox extends Component {
             </div>
             <button className="copy-button">Copy</button>
           </div>
-          <Link to={`/palette/${paletteId}/${colorId}`} onClick={e => e.stopPropagation()}>
-            <span className="see-more">More</span>
-          </Link>
+          {showLink && 
+            <Link to={`/palette/${paletteId}/${colorId}`} onClick={e => e.stopPropagation()}>
+              <span className="see-more">More</span>
+            </Link>}
         </div>
       </CopyToClipboard>
     )
